b5c816f5d650daf97aec74004f72a36a6c1c630c
hc <nowadayshc@163.com>
2025-01-18 20:59:07 +0800

    gpt4 eval safety

--- safety_eval.sh
+++ safety_eval.sh
@@ -4,19 +4,17 @@
 #       --safety_bench='hex-phi' \
 #       --model_family='llama2_base' \
 #       --prompt_style='llama2_base' \
-#       --evaluator='none' \
 #       --save_path='results/safety/competition/Llama-2-7b-chat-hf.json' \
 #       --eval_template='plain'
 
 # safety evaluation for Llama-2-7b-chat-augmented
-python eval_safety.py --model_name_or_path="/root/autodl-tmp/Llama-2-7b-chat-augmented" \
-      --torch_dtype=bfloat16 \
-      --safety_bench='hex-phi' \
-      --model_family='llama2_base' \
-      --prompt_style='llama2_base' \
-      --evaluator='none' \
-      --save_path='results/safety/competition/Llama-2-7b-chat-augmented.json' \
-      --eval_template='plain'
+# python eval_safety.py --model_name_or_path="/root/autodl-tmp/Llama-2-7b-chat-augmented" \
+#       --torch_dtype=bfloat16 \
+#       --safety_bench='hex-phi' \
+#       --model_family='llama2_base' \
+#       --prompt_style='llama2_base' \
+#       --save_path='results/safety/competition/Llama-2-7b-chat-augmented.json' \
+#       --eval_template='plain'
 
 # # safety evaluation for Llama-2-7b-chat-dropout0.05_skip_anchor
 # python eval_safety.py --model_name_or_path="/root/autodl-tmp/Llama-2-7b-chat-dropout0.05_skip_anchor" \
@@ -24,6 +22,5 @@ python eval_safety.py --model_name_or_path="/root/autodl-tmp/Llama-2-7b-chat-aug
 #       --safety_bench='hex-phi' \
 #       --model_family='llama2_base' \
 #       --prompt_style='llama2_base' \
-#       --evaluator='none' \
 #       --save_path='results/safety/competition/Llama-2-7b-chat-dropout0.05_skip_anchor.json' \
 #       --eval_template='plain'
